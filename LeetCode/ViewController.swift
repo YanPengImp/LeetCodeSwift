cb25651113311d4f42d628149dd1e55fcaf2c4f0
imp <yp@jingbo360.com>
2018-06-28 13:40:35 +0800

    Initial Commit

---
+++ LeetCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LeetCode
+//
+//  Created by Imp on 2018/6/28.
+//  Copyright © 2018年 jingbo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
